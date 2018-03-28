1de9f05c06a55870835f3786ce0f91c38010fb24
Yu-Jen Chang <juzen2003@gmail.com>
2018-03-28 11:57:28 -0700

    Finish Jbuilder exercise

---
+++ skeleton/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Your routes here!
+  namespace :api, defaults: { format: :json } do
+  # Your routes here
+    resources :guests, only: [:show, :index] do
+      resources :gifts, only: [:index]
+    end
+    resources :parties, only: [:show, :index]
+
+    resources :gifts, only: [:show]
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
